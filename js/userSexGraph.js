33bcf672e0ff4a2ab2804a37ea566b93d5a09a4c
Demiddel-Jesse <jesse.demiddel@student.howest.be>
2022-12-10 12:05:05 +0100

    removed chart.js

--- js/userSexGraph.js
+++ js/userSexGraph.js
@@ -1,30 +1,7 @@
 'use strict';
-import Chart from '/node_modules/chart.js/auto/auto.js';
-
-const ctx = document.getElementById('myChart');
-
-const dataset = {
-	labels: ['Man', 'Vrouw', 'Non-binair'],
-	datasets: [
-		{
-			label: 'User Sex',
-			data: [2490, 2490, 20],
-			backgroundColor: ['#14191E', '#00f0c8', '#dcdcdc'],
-			hoverOffset: 4,
-		},
-	],
-};
-
-const createChart = function () {
-	new Chart(ctx, {
-		type: 'doughnut',
-		data: dataset,
-	});
-};
 
 const init = function () {
 	console.log('initialized');
-	createChart();
 };
 
 document.addEventListener('DOMContentLoaded', init);
