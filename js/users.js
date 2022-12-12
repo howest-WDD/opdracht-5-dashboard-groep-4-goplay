311e7d7d68212845aac56aa522f3da4cd912d5c1
mathieu buysse <mathieu.buysse@student.howest.be>
2022-12-12 20:45:06 +0100

    commit finished for the day

--- js/users.js
+++ js/users.js
@@ -13,8 +13,44 @@ const listenToExpand = function(){
     })
 }
 
+const listenToEdit= function(){
+    const edit = document.querySelector(".js-edit");
+    const inputs = document.getElementsByClassName("js-disable")
+    console.log(inputs)
+    console.log(edit)
+
+    edit.addEventListener("click",function(){
+        for(const input of inputs){
+            input.toggleAttribute("disabled")
+                
+        }
+        console.log("edit")
+    })
+       
+    
+}
+
+const listenTodelete = function(){
+    const deletebutton = document.querySelector(".js-delete")
+    const deleteusers = document.getElementsByClassName("js-deleteuser")
+    console.log(deletebutton)
+    console.log(deleteusers)
+
+    deletebutton.addEventListener("click",function(){
+        for(const deleteuser of deleteusers){
+            console.log(deleteuser)
+            console.log("click")
+            deleteuser.classList.add("u-hidden")
+        }
+        
+
+    })
+}
+
 const accordeonUsers = function(){
-    listenToExpand()
+    listenToExpand();
+    listenToEdit();
+    listenTodelete();
     console.log("startup")
 }
 
