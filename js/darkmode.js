045f1b895c351bc428523c11edec2867bc103a3e
Nathan Dhaene <nathan.dhaene@student.howest.be>
2022-12-14 15:49:42 +0100

    darkmode tweaks

--- js/darkmode.js
+++ js/darkmode.js
@@ -3,10 +3,15 @@
   const toggleDarkmode = function(){
     const body = document.body;
     const logo = document.querySelectorAll('.js-logo')
+    const button = document.querySelector('.js-darkmode');
+    const dark = document.querySelector('.c-button2');
   
     body.classList.toggle('o-darkmode');
-    logo[0].classList.toggle('u-hidden')
-    logo[1].classList.toggle('u-hidden')
+    logo[0].classList.toggle('u-hidden');
+    logo[1].classList.toggle('u-hidden');
+    button.classList.toggle('c-button2');
+    button.classList.toggle('c-button');
+    dark.innerHTML = `Dark mode`;
     
   
   };
