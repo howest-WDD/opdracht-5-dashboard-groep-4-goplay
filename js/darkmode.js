36f42385ecd7bda8fff366f7a49d62e3efe2a1b9
Nathan Dhaene <nathan.dhaene@student.howest.be>
2022-12-15 08:46:33 +0100

    js darkmode changes text

--- js/darkmode.js
+++ js/darkmode.js
@@ -1,17 +1,28 @@
 "use strict";
+
+  const toggleText = function(){
+    const dark = document.querySelector('.js-darkmode');
+
+    if (dark.innerHTML === "Dark mode") {
+      dark.innerHTML = "Light mode";
+    } else {
+      dark.innerHTML = "Dark mode";
+    }
+
+  };
   
   const toggleDarkmode = function(){
     const body = document.body;
     const logo = document.querySelectorAll('.js-logo')
     const button = document.querySelector('.js-darkmode');
-    const dark = document.querySelector('.c-button2');
+    
   
     body.classList.toggle('o-darkmode');
     logo[0].classList.toggle('u-hidden');
     logo[1].classList.toggle('u-hidden');
     button.classList.toggle('c-button2');
-    button.classList.toggle('c-button');
-    dark.innerHTML = `Dark mode`;
+    button.classList.toggle('c-button');  
+    toggleText(); 
     
   
   };
