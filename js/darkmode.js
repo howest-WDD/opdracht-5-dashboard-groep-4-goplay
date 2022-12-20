39f8e1d3f6fa0b4611df5ed13e57d99fb491918a
Demiddel-Jesse <jesse.demiddel@student.howest.be>
2022-12-20 18:23:27 +0100

    currently disabled listenToToggle

--- js/darkmode.js
+++ js/darkmode.js
@@ -1,41 +1,36 @@
-"use strict";
+'use strict';
 
-  const toggleText = function(){
-    const dark = document.querySelector('.js-darkmode');
+const toggleText = function () {
+	const dark = document.querySelector('.js-darkmode');
 
-    if (dark.innerHTML === "Dark mode") {
-      dark.innerHTML = "Light mode";
-    } else {
-      dark.innerHTML = "Dark mode";
-    }
+	if (dark.innerHTML === 'Dark mode') {
+		dark.innerHTML = 'Light mode';
+	} else {
+		dark.innerHTML = 'Dark mode';
+	}
+};
 
-  };
-  
-  const toggleDarkmode = function(){
-    const body = document.body;
-    const logo = document.querySelectorAll('.js-logo')
-    const button = document.querySelector('.js-darkmode');
-    
-  
-    body.classList.toggle('o-darkmode');
-    logo[0].classList.toggle('u-hidden');
-    logo[1].classList.toggle('u-hidden');
-    button.classList.toggle('c-button2');
-    button.classList.toggle('c-button');  
-    toggleText(); 
-    
-  
-  };
-  
-  const lisentoToggle = function(){
-     const toggle = document.querySelector('.js-darkmode');
-  
-     toggle.addEventListener("click", toggleDarkmode);
-  };
-  
-  const init = function () {
-    lisentoToggle();
-  };
-  
-  document.addEventListener("DOMContentLoaded", init);
-  
+const toggleDarkmode = function () {
+	const body = document.body;
+	const logo = document.querySelectorAll('.js-logo');
+	const button = document.querySelector('.js-darkmode');
+
+	body.classList.toggle('o-darkmode');
+	logo[0].classList.toggle('u-hidden');
+	logo[1].classList.toggle('u-hidden');
+	button.classList.toggle('c-button2');
+	button.classList.toggle('c-button');
+	toggleText();
+};
+
+const lisentoToggle = function () {
+	const toggle = document.querySelector('.js-darkmode');
+
+	toggle.addEventListener('click', toggleDarkmode);
+};
+
+const init = function () {
+	// lisentoToggle();
+};
+
+document.addEventListener('DOMContentLoaded', init);
