e04d48efa7609d1b8dcd21104abe01e2539738ec
mathieu buysse <mathieu.buysse@student.howest.be>
2022-12-20 13:30:12 +0100

    modal window fixes

--- js/users.js
+++ js/users.js
@@ -32,7 +32,7 @@ const loadJson = function () {
 				// console.log(getExtra.innerHTML)
 				htmlString += `
                 
-                    <table  class="c-users__table js-" style="overflow-x:auto;">
+                    <table  class="c-users__table " style="overflow-x:auto;">
                         <tr id="menu" class="c-users__tablerow c-users__tablerow--first  js-deleteuser js-adduser ">
                             <td class="c-users__category js-id"><input class="c-users__input  js-adduser  js-geboortedatum js-disable" type="text" placeholder="${id}" disabled /></td>
                             <td class="c-users__category js-achternaam"><input class="c-users__input js-adduser  js-geboortedatum js-disable" type="text" placeholder="${achternaam}" disabled /></td>
@@ -46,21 +46,21 @@ const loadJson = function () {
                                     </svg>
                                 </button>
                             </td>
-                            <div id="extramenu" class="c-users__extracontainer js-addspacetop js-deleteuser" data-space="${id}" style="overflow-x:auto;"> 	 	
-                                <td id="js-extra${id}" class="c-users__extradata u-hidden  d-flex js-extra " data-spacetop="${id}">
+                            <div id="extramenu" class="c-users__extracontainer js-deleteuser"  style="overflow-x:auto;"> 	 	
+                                <td id="js-extra${id}" class="c-users__extradata u-hidden  d-flex js-extra " >
                                     <div class="c-users__extrainformatie c-users__extrainformatie--1 ">
 
                                         <div class="c-users__geboortedatum">
                                             <label for="geboortedatum" class="c-users__label c-users__align1">Geboortedatum:</label>
-                                            <input type="text" id="geboortedatum" class=" c-users__input js-geboortedatum js-adduser  js-disable" placeholder="${geboortedatum}" name="geboortedatum" disabled />
+                                            <input type="text" id="geboortedatum" class=" c-users__input js-geboortedatum js-adduser  js-disable" placeholder="${geboortedatum}" data-disabledid="${id}" id="edit-input${id}" name="geboortedatum" disabled />
                                         </div>
                                         <div class="c-users__straatnaam">
                                             <label for="straatnaam" class="c-users__label c-users__align2">Straatnaam:</label>
-                                            <input type="text" id="straatnaam" class="c-users__input js-straatnaam js-disable" placeholder="${straatnaam}" name="straatnaam" disabled />
+                                            <input type="text" id="straatnaam" class="c-users__input js-straatnaam js-disable" placeholder="${straatnaam}" data-disabledid="${id}" id="edit-input${id}" name="straatnaam" disabled />
                                         </div>
                                         <div class="c-users__nieuwsbrief">
                                             <label for="nieuwsbrief" class="c-users__label  c-users__align3">Nieuwsbrief:</label>
-                                            <select name="jaofnee" class="c-users__select js-nieuwsbrief js-disable" id="nieuwsbrief" disabled>
+                                            <select name="jaofnee" class="c-users__select js-nieuwsbrief js-disable" id="nieuwsbrief" data-disabledid="${id}" id="edit-input${id}" disabled>
                                                 <option value="ja" class="c-users__option js-disable">ja</option>
                                                 <option value="nee" class="c-users__option js-disable">nee</option>
                                             </select>
@@ -69,15 +69,15 @@ const loadJson = function () {
                                     <div class="c-users__extrainformatie c-users__extrainformatie--2">
                                         <div class="c-users__postcode">
                                             <label for="postcode" class="c-users__label  c-users__align1">Postcode:</label>
-                                            <input type="text" id="postcode" class="c-users__input js-postcode js-disable" placeholder="${postcode}" name="postcode" disabled />
+                                            <input type="text" id="postcode" class="c-users__input js-postcode js-disable" placeholder="${postcode}" data-disabledid="${id}" id="edit-input${id}" name="postcode" disabled />
                                         </div>
                                         <div class="c-users__nr">
                                             <label for="nr" class="c-users__label c-users__align2">Nr:</label>
-                                            <input type="text" id="nr" class="c-users__input js-nr js-disable" placeholder="${nr}" name="nr" disabled />
+                                            <input type="text" id="nr" class="c-users__input js-nr js-disable" placeholder="${nr}" name="nr" disabled data-disabledid="${id}" id="edit-input${id}" />
                                         </div>
                                         <div class="c-users__reclame">
                                             <label for="reclame" class="c-users__label c-users__align3">Reclame:</label>
-                                            <select name="jaofnee" class="c-users__select js-reclame js-disable" disabled id="reclame">
+                                            <select name="jaofnee" class="c-users__select js-reclame js-disable" disabled  data-disabledid="${id}" id="edit-input${id}">
                                                 <option value="ja" class="c-users__option">ja</option>
                                                 <option value="nee" class="c-users__option">nee</option>
                                             </select>
@@ -86,15 +86,15 @@ const loadJson = function () {
                                     <div class="c-users__extrainformatie c-users__extrainformatie--3">
                                         <div class="c-users__gemeente">
                                             <label for="gemeente" class="c-users__label  c-users__align1">Gemeente:</label>
-                                            <input type="text" id="gemeente" class="c-users__input js-gemeente js-disable" placeholder="${gemeente}" name="gemeente" disabled />
+                                            <input type="text" id="gemeente" class="c-users__input js-gemeente js-disable" placeholder="${gemeente}" data-disabledid="${id}" id="edit-input${id}" name="gemeente" disabled />
                                         </div>
                                         <div class="c-users__bus">
                                             <label for="bus" class="c-users__label c-users__align2">Bus:</label>
-                                            <input type="text" id="bus" class="c-users__input js-bus js-disable" placeholder="${bus}" name="bus" disabled />
+                                            <input type="text" id="bus" class="c-users__input js-bus js-disable" placeholder="${bus}" data-disabledid="${id}"id="edit-input${id}" name="bus" disabled />
                                         </div>
                                         <div class="c-users__isadmin">
                                             <label class="c-users__label c-users__align3">IsAdmin:</label>
-                                            <select name="jaofnee" class="c-users__select js-admin js-disable" disabled id="ja">
+                                            <select name="jaofnee" class="c-users__select js-admin js-disable" data-disabledid="${id}"id="edit-input${id}" disabled id="ja">
                                                 <option value="ja" class="c-users__option">ja</option>
                                                 <option value="nee" class="c-users__option">nee</option>
                                             </select>
@@ -103,11 +103,11 @@ const loadJson = function () {
                                     <div class="c-users__extrainformatie  c-users__extrainformatie--4">
                                         <div class="c-users__telefoonnummer">
                                             <label for="telefoonnummer" class="c-users__label c-users__align1">Telefoonnummer:</label>
-                                            <input type="text" id="telefoonnummer" class="c-users__input js-telefoonnummer js-disable" placeholder="${phone}" name="telefoonnummer" disabled />
+                                            <input type="text" id="telefoonnummer" class="c-users__input js-telefoonnummer js-disable" placeholder="${phone}" name="telefoonnummer"id="edit-input${id}" data-disabledid="${id}" disabled />
                                         </div>
                                         <div class="c-users__geslacht">
                                             <label for="geslacht" class="c-users__label  c-users__align2">Geslacht:</label>
-                                            <input type="text" id="geslacht" class=" c-users__input js-geslacht js-disable" placeholder="${geslacht}" name="geslacht" disabled />
+                                            <input type="text" id="geslacht" class=" c-users__input js-geslacht js-disable" placeholder="${geslacht}" id="edit-input${id}" name="geslacht" data-disabledid="${id}" disabled />
                                         </div>
                                         <div class="c-users__telefoonnummer"></div>
                                     </div>
@@ -117,7 +117,7 @@ const loadJson = function () {
                                                 <path fill="currentColor" class="c-users__deletepath" d="M6,19A2,2 0 0,0 8,21H16A2,2 0 0,0 18,19V7H6V19M8.46,11.88L9.87,10.47L12,12.59L14.12,10.47L15.53,11.88L13.41,14L15.53,16.12L14.12,17.53L12,15.41L9.88,17.53L8.47,16.12L10.59,14L8.46,11.88M15.5,4L14.5,3H9.5L8.5,4H5V6H19V4H15.5Z" />
                                             </svg>
                                         </button>
-                                        <button class="c-users__edit js-edit" data-editid="${id}">
+                                        <button class="c-users__edit js-edit" id="js-editid${id}" data-editid="${id}">
                                             <svg style="width: 24px; height: 24px" class="c-users__editsvg c-users__editsvg--edit js-editsvg" viewBox="0 0 24 24">
                                                 <path fill="currentColor" class="c-users__editpath" d="M20.71,7.04C21.1,6.65 21.1,6 20.71,5.63L18.37,3.29C18,2.9 17.35,2.9 16.96,3.29L15.12,5.12L18.87,8.87M3,17.25V21H6.75L17.81,9.93L14.06,6.18L3,17.25Z" />
                                             </svg>
@@ -129,56 +129,53 @@ const loadJson = function () {
                     
                     </table>`;
 
-
-
 				// console.log(htmlString);
 			}
 			getElement.innerHTML += htmlString;
 
 			// console.log(htmlString)
 		});
-
-		
 };
 
 const listenToExpand = function () {
-    const expandButtons = document.querySelectorAll(`.js-expand`);
-    const expandSvg = document.querySelector('.js-expandbutton');
-	const spacetop = document.querySelector(`.js-addspacetop`);
-    for (const expandButton of expandButtons) {
-        // console.log(expandButton);
-        expandButton.addEventListener('click', function () {
-            const idnumber = expandButton.getAttribute('data-id');
-			const spacenumbers = spacetop.getAttribute('data-space')
-            const extraTexts = document.getElementById(`js-extra${idnumber}`);		
+	const expandButtons = document.querySelectorAll(`.js-expand`);
+	const expandSvg = document.querySelector(".js-expandbutton");
+
+	for (const expandButton of expandButtons) {
+		// console.log(expandButton);
+		expandButton.addEventListener("click", function () {
+			const idnumber = expandButton.getAttribute("data-id");
+			
+			const extraTexts = document.getElementById(`js-extra${idnumber}`);
 			extraTexts.classList.toggle("u-hidden");
-			extraTexts.classList.toggle("c-spacetop")
+		
 
-
-
-            // console.log('click');
-            // console.log(this.getAttribute('data-id'));
-            // console.log(expandButton);
-            // console.log(extraTexts);
+			// console.log('click');
+			// console.log(this.getAttribute('data-id'));
+			// console.log(expandButton);
+			// console.log(extraTexts);
 		});
-
-    }
+	}
 };
 const listenToEdit = function () {
 	const editusers = document.querySelectorAll(".js-edit");
 	const inputs = document.getElementsByClassName("js-disable");
-	// console.log(editusers)
-	
-	// console.log(inputs);
-	// console.log(edit);
+	for (const input of inputs) {
+		for (const edituser of editusers) {
+			console.log(edituser);
 
+			edituser.addEventListener("click", function () {
+				console.log("click");
 
-	// edit.addEventListener("click", function () {
-	// 	for (const input of inputs) {
-	// 		input.toggleAttribute("disabled");
-	// 	}
-	// 	// console.log("edit")
-	// });
+				const deleteid = edituser.getAttribute("data-editid");
+				// console.log(deleteid);
+				const disable = document.getElementById(`js-editid${deleteid}`);
+				// console.log(disable);
+
+				input.toggleAttribute("disabled");
+			});
+		}
+	}
 };
 
 const listenTodelete = function () {
@@ -190,24 +187,12 @@ const listenTodelete = function () {
 	const modal = document.querySelector(".js-modal");
 	const verwijder = document.querySelector(".js-verwijder");
 	const cancel = document.querySelector(".js-cancel");
-
-	
-	// console.log(deletebutton);
-	console.log(deleteusers);
-	console.log(deletebuttons)
-
-	// deletebutton.addEventListener("click", function () {
-	// 	modal.classList.remove("u-hidden");
-	// 	darkbackground.classList.add("c-darkbackground");
-
-	// 	// for(const deleteuser of deleteusers){
-
-	// 	//     // console.log(deleteuser)
-	// 	//     // console.log("click")
-	// 	//     deleteuser.classList.add("u-hidden")
-
-	// 	// }
-	// });
+	for(const deletebutton of deletebuttons){
+		deletebutton.addEventListener("click", function () {
+			modal.classList.remove("u-hidden");
+			darkbackground.classList.add("c-darkbackground");
+		});
+	}
 	verwijder.addEventListener("click", function () {
 		for (const deleteuser of deleteusers) {
 			deleteuser.classList.add("u-hidden");
@@ -222,20 +207,23 @@ const listenTodelete = function () {
 };
 
 const listenToEditChange = function () {
-	const editButton = document.querySelector(".js-edit");
-	const editSvg = document.querySelector(".js-editsvg");
-
-	editButton.addEventListener("click", function () {
-		if (editSvg.classList.contains("c-users__editsvg--edit")) {
-			editSvg.classList.remove("c-users__editsvg--edit");
-			editSvg.classList.add("c-users__editsvg--save");
-			editSvg.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" class="c-users__editsvg" viewBox="0 0 24 24"><path class="c-users__editpath" d="M15,9H5V5H15M12,19A3,3 0 0,1 9,16A3,3 0 0,1 12,13A3,3 0 0,1 15,16A3,3 0 0,1 12,19M17,3H5C3.89,3 3,3.9 3,5V19A2,2 0 0,0 5,21H19A2,2 0 0,0 21,19V7L17,3Z" /></svg>`;
-		} else if (editSvg.classList.contains("c-users__editsvg--save")) {
-			editSvg.classList.add("c-users__editsvg--edit");
-			editSvg.classList.remove("c-users__editsvg--save");
-			editSvg.innerHTML = `<svg style="width: 24px; height: 24px" class="c-users__editsvg c-users__editsvg--edit js-editsvg" viewBox="0 0 24 24"><path fill="currentColor" class="c-users__editpath" d="M20.71,7.04C21.1,6.65 21.1,6 20.71,5.63L18.37,3.29C18,2.9 17.35,2.9 16.96,3.29L15.12,5.12L18.87,8.87M3,17.25V21H6.75L17.81,9.93L14.06,6.18L3,17.25Z" /></svg>`;
+	const editButtons = document.querySelectorAll(".js-edit");
+	const editSvgs = document.querySelectorAll(".js-editsvg");
+	for (const editButton of editButtons) {
+		for (const editSvg of editSvgs) {
+			editButton.addEventListener("click", function () {
+				if (editSvg.classList.contains("c-users__editsvg--edit")) {
+					editSvg.classList.remove("c-users__editsvg--edit");
+					editSvg.classList.add("c-users__editsvg--save");
+					editSvg.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" class="c-users__editsvg" viewBox="0 0 24 24"><path class="c-users__editpath" d="M15,9H5V5H15M12,19A3,3 0 0,1 9,16A3,3 0 0,1 12,13A3,3 0 0,1 15,16A3,3 0 0,1 12,19M17,3H5C3.89,3 3,3.9 3,5V19A2,2 0 0,0 5,21H19A2,2 0 0,0 21,19V7L17,3Z" /></svg>`;
+				} else if (editSvg.classList.contains("c-users__editsvg--save")) {
+					editSvg.classList.add("c-users__editsvg--edit");
+					editSvg.classList.remove("c-users__editsvg--save");
+					editSvg.innerHTML = `<svg style="width: 24px; height: 24px" class="c-users__editsvg c-users__editsvg--edit js-editsvg" viewBox="0 0 24 24"><path fill="currentColor" class="c-users__editpath" d="M20.71,7.04C21.1,6.65 21.1,6 20.71,5.63L18.37,3.29C18,2.9 17.35,2.9 16.96,3.29L15.12,5.12L18.87,8.87M3,17.25V21H6.75L17.81,9.93L14.06,6.18L3,17.25Z" /></svg>`;
+				}
+			});
 		}
-	});
+	}
 };
 const filterbuttons = function () {
 	const thbuttons = document.querySelectorAll(".js-filter");
@@ -247,7 +235,7 @@ const filterbuttons = function () {
 				thbutton.classList.remove("c-users__id--active");
 			}
 			// console.log("click")
-			thbutton.classList.add("c-users__id--active");
+			thbutton.classList.toggle("c-users__id--active");
 			thbutton.innerHTML += `<svg xmlns="http://www.w3.org/2000/svg" class="c-users__activesvg" viewBox="0 0 24 24"><path class="c-users__activesvgpath" d="M7,10L12,15L17,10H7Z" /></svg>`;
 		});
 	}
@@ -256,10 +244,10 @@ const filterbuttons = function () {
 const accordeonUsers = function () {
 	loadJson();
 	setTimeout(listenToExpand, 1000);
-	setTimeout(listenToEditChange,1000)
-	setTimeout(listenTodelete,1000)
-	setTimeout(listenToEdit,1000)
-	setTimeout(filterbuttons,1000)
+	setTimeout(listenToEditChange, 1000);
+	setTimeout(listenTodelete, 1000);
+	setTimeout(listenToEdit, 1000);
+	setTimeout(filterbuttons, 1000);
 };
 
 document.addEventListener("DOMContentLoaded", accordeonUsers());
