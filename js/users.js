06ab92d9f5d900115b7b3ff695cdf87891d0b45a
mathieu buysse <mathieu.buysse@student.howest.be>
2022-12-20 17:44:24 +0100

    bedankt papa markup

--- js/users.js
+++ js/users.js
@@ -105,7 +105,7 @@ const loadJson = function () {
                                         </div>
                                         <div class="c-users__telefoonnummer"></div>
                                     </div>
-                                    <div>
+                                    <div class="c-users__editbuttoncontainer">
                                         <button class="c-users__delete js-delete" data-deleteid="${id}">
                                             <svg style="width: 24px; height: 24px" class="c-users__deletesvg" viewBox="0 0 24 24">
                                                 <path fill="currentColor" class="c-users__deletepath" d="M6,19A2,2 0 0,0 8,21H16A2,2 0 0,0 18,19V7H6V19M8.46,11.88L9.87,10.47L12,12.59L14.12,10.47L15.53,11.88L13.41,14L15.53,16.12L14.12,17.53L12,15.41L9.88,17.53L8.47,16.12L10.59,14L8.46,11.88M15.5,4L14.5,3H9.5L8.5,4H5V6H19V4H15.5Z" />
@@ -301,12 +301,19 @@ const filterbuttons = function () {
 };
 
 const accordeonUsers = function () {
-	loadJson();
-	setTimeout(listenToExpand, 1000);
-	setTimeout(listenToEditChange, 1000);
-	setTimeout(listenTodelete, 1000);
-	setTimeout(listenToEdit, 1000);
-	setTimeout(filterbuttons, 1000);
+
+	if(document.getElementById("menu")){
+		loadJson();
+		setTimeout(listenToExpand, 1000);
+		setTimeout(listenToEditChange, 1000);
+		setTimeout(listenTodelete, 1000);
+		setTimeout(listenToEdit, 1000);
+		setTimeout(filterbuttons, 1000);
+	} else{
+		console.log("no menu")
+	}
+
+
 };
 
 document.addEventListener('DOMContentLoaded', accordeonUsers());
