b876937ac68ae264d02d0a66d45291f31d422fae
vanherpyannis <yannis.van.herp@student.howest.be>
2022-12-28 19:11:05 +0100

    added js to toggle modal and rewritten the url splitting to check on which page you are

--- js/contentModal.js
+++ js/contentModal.js
@@ -1,11 +1,21 @@
+const modalContentToggle = function () {
+    const modal = document.querySelector("#modalContent");
+    const classes = modal.classList
+    if (classes.contains('u-hidden')) {
+        modal.classList.remove("u-hidden");
+    } else {
+        modal.classList.add("u-hidden");
+    }
+};
+
 const loadContentModal = function () {
-    const url = window.location.href;
-    if (url === "http://127.0.0.1:8080/edit_movie.html") {
+    const url = window.location.href.split("/")[3];
+    if (url === "edit_movie.html" || url === "edit_serie.html") {
         // check if you are on the movie editing page
-
-
-    } else if (url === "http://127.0.0.1:8080/edit_serie.html") {
-        // check if you are on the serie editing page
+        const editButton = document.querySelector("#editMovies");
+        const cancelButton = document.querySelector("#cancelButton");
+        editButton.addEventListener('click', modalContentToggle);
+        cancelButton.addEventListener('click', modalContentToggle);
     } else {
         //do nothing cause not on the right page
     }
