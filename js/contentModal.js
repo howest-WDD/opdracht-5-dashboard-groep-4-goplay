a005bea0a63e75777b99325f94f3bd9c7f8c7db4
vanherpyannis <yannis.van.herp@student.howest.be>
2022-12-29 11:01:45 +0100

    fixed small bug to check when the modalwindow needs to open

--- js/contentModal.js
+++ js/contentModal.js
@@ -1,17 +1,23 @@
 const modalContentToggle = function () {
+    //select the modal window
     const modal = document.querySelector("#modalContent");
     const classes = modal.classList
+    //makes the modal window visible if clicked on edit button
     if (classes.contains('u-hidden')) {
         modal.classList.remove("u-hidden");
-    } else {
+    }
+    //makes the modal window not visible on cancel
+    else {
         modal.classList.add("u-hidden");
     }
 };
 
 const loadContentModal = function () {
-    const url = window.location.href.split("/")[3];
-    if (url === "edit_movie.html" || url === "edit_serie.html") {
-        // check if you are on the movie editing page
+    //split the url and take the 3 parameter
+    const url = window.location.href;
+    // check if you are on the movie/serie editing page
+    if (url.includes("edit_movie.html") || url.includes("edit_serie.html")) {
+        //select the buttons and check for click events
         const editButton = document.querySelector("#editMovies");
         const cancelButton = document.querySelector("#cancelButton");
         editButton.addEventListener('click', modalContentToggle);
