67695f33af554c6b052be015c0f63bd45bad884a
vanherpyannis <yannis.van.herp@student.howest.be>
2023-01-08 14:59:18 +0100

    added loading logo for data and rewritten the way i fetch the data

--- js/displayContent.js
+++ js/displayContent.js
@@ -1,30 +1,26 @@
 //#region ***  DOM references ***
-
+let loader;
 //#endregion
-
-const showTvShows = function (jsonObject) {
-    for (const show of jsonObject) {
-        let htmlstring_show = `
-            <a class="c-content-card" href="#" data-id="${show._id}">
-            <figure class="c-content-card__figure">
-                <img class="c-content-card__figure--img" src="${show.thumbnailImage}" alt="Image" />
-                <figcaption class="c-content-card__figure--text">${show.title}</figcaption>
-            </figure>
-        </a>`
-        document.querySelector(".js-content").innerHTML += htmlstring_show;
+// showing loading
+function displayLoading() {
+    if (loader.classList.contains("u-hidden")) {
+        loader.classList.remove("u-hidden");
+    } else {
+        loader.classList.add("u-hidden");
     }
+
 }
 
-const showMovies = function (jsonObject) {
-    for (const movie of jsonObject) {
-        let htmlstring_movie = `
-            <a class="c-content-card" href="#" data-id="${movie._id}">
+const showContent = function (jsonObject) {
+    for (const content of jsonObject) {
+        let htmlstring_content = `
+            <a class="c-content-card" href="#" data-id="${content._id}">
             <figure class="c-content-card__figure">
-                <img class="c-content-card__figure--img" src="${movie.tumbnailImage}" alt="Image" />
-                <figcaption class="c-content-card__figure--text">${movie.title}</figcaption>
+                <img class="c-content-card__figure--img" src="${content.thumbnailImage}" alt="Image" />
+                <figcaption class="c-content-card__figure--text">${content.title}</figcaption>
             </figure>
         </a>`
-        document.querySelector(".js-content").innerHTML += htmlstring_movie;
+        document.querySelector(".js-content").innerHTML += htmlstring_content;
     }
 }
 
@@ -36,12 +32,27 @@ const getMovies = function () {
     handleData("https://goplayhowestapifunction.azurewebsites.net/api/getmovies", showMovies);
 }
 
+//handles the data Array if you fetch all the data
+const handleAllData = function (jsonObject) {
+    for (const content of jsonObject) {
+        showContent(content)
+    }
+    displayLoading()
+}
+
+const getAllContent = function () {
+    displayLoading()
+    Promise.all(["getmovies", "getshows"].map(name =>
+        fetch(`https://goplayhowestapifunction.azurewebsites.net/api/${name}`).then(resp => resp.json())
+    )).then(handleAllData)
+}
+
 const displayContent = function () {
     const url = window.location.href;
     // check if you are on the content page
     if (url.includes("content.html")) {
-        getTvShows()
-        getMovies()
+        loader = document.querySelector("#loading");
+        getAllContent();
     } else {
         //do nothing cause not on the right page
     }
