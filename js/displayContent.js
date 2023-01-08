729311bebc75ab406ac85c0db2667b0979b572d3
vanherpyannis <yannis.van.herp@student.howest.be>
2023-01-08 16:32:22 +0100

    added search functionality

--- js/displayContent.js
+++ js/displayContent.js
@@ -1,26 +1,59 @@
 //#region ***  DOM references ***
-let loader;
+let loader, dataStore = [], searchbar;
 //#endregion
-// showing loading
+
+// showing loading logo
 function displayLoading() {
     if (loader.classList.contains("u-hidden")) {
         loader.classList.remove("u-hidden");
     } else {
         loader.classList.add("u-hidden");
     }
-
 }
 
 const showContent = function (jsonObject) {
     for (const content of jsonObject) {
         let htmlstring_content = `
-            <a class="c-content-card" href="#" data-id="${content._id}">
+            <a class="c-content-card js-card" href="#" data-id="${content._id}" data-type="${content.seasons}">
             <figure class="c-content-card__figure">
                 <img class="c-content-card__figure--img" src="${content.thumbnailImage}" alt="Image" />
                 <figcaption class="c-content-card__figure--text">${content.title}</figcaption>
             </figure>
         </a>`
         document.querySelector(".js-content").innerHTML += htmlstring_content;
+    }
+    listenToClickOnCard();
+}
+
+const showFilteredContent = function (data) {
+    document.querySelector(".js-content").innerHTML = "";
+    for (const content of data) {
+        let htmlstring_content = `
+            <a class="c-content-card js-card" href="#" data-id="${content._id}" data-type="${content.seasons}">
+            <figure class="c-content-card__figure">
+                <img class="c-content-card__figure--img" src="${content.thumbnailImage}" alt="Image" />
+                <figcaption class="c-content-card__figure--text">${content.title}</figcaption>
+            </figure>
+        </a>`
+        document.querySelector(".js-content").innerHTML += htmlstring_content;
+    }
+    listenToClickOnCard();
+}
+
+const listenToClickOnCard = function () {
+    const cards = document.querySelectorAll(".js-card");
+    for (const card of cards) {
+        card.addEventListener("click", function () {
+            let id = card.getAttribute("data-id");
+            let type = card.getAttribute("data-type");
+            if (type === "undefined") {
+                window.location.href = `http://127.0.0.1:8080/edit_movie.html?${id}`;
+            } else if (type === "[object Object]") {
+                window.location.href = `http://127.0.0.1:8080/edit_serie.html?${id}`;
+            } else {
+                console.log("euh no idea what this is");
+            }
+        })
     }
 }
 
@@ -32,10 +65,11 @@ const getMovies = function () {
     handleData("https://goplayhowestapifunction.azurewebsites.net/api/getmovies", showMovies);
 }
 
-//handles the data Array if you fetch all the data
+//handles the data Array if you fetch all the data and puts it in an Array
 const handleAllData = function (jsonObject) {
     for (const content of jsonObject) {
         showContent(content)
+        dataStore.push(content)
     }
     displayLoading()
 }
@@ -52,7 +86,16 @@ const displayContent = function () {
     // check if you are on the content page
     if (url.includes("content.html")) {
         loader = document.querySelector("#loading");
+        searchbar = document.querySelector("#filterInput");
         getAllContent();
+
+        searchbar.addEventListener('keyup', function (text) {
+            const currentword = text.target.value;
+            const filteredMovies = dataStore[0].filter(data => data.title.includes(currentword));
+            const filteredSeries = dataStore[1].filter(data => data.title.includes(currentword));
+            const filteredData = filteredMovies.concat(filteredSeries);
+            showFilteredContent(filteredData)
+        });
     } else {
         //do nothing cause not on the right page
     }
