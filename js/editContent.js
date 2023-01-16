4c19070d798f7ea400c5c184f1f8ea9fe34b37ff
vanherpyannis <yannis.van.herp@student.howest.be>
2023-01-16 23:51:35 +0100

    added all the new pages

--- js/editContent.js
+++ js/editContent.js
@@ -1,97 +1,5 @@
-let html_movie, html_show;
-
-const modalContentToggle = function () {
-    //select the modal window
-    const modal = document.querySelector("#editContentModal");
-    const classes = modal.classList
-    //makes the modal window visible if clicked on edit button
-    if (classes.contains('u-hidden')) {
-        modal.classList.remove("u-hidden");
-    }
-    //makes the modal window not visible on cancel
-    else {
-        modal.classList.add("u-hidden");
-    }
-};
-
-const showShow = function (jsonObject) {
-    console.log(jsonObject)
-    html_show.innerHTML = ""
-    let htmlstring_show = `
-            <div class="program-header">
-            <div class="program-header__inner">
-                <header class="hero-video has-playlists">
-                    <div class="hero-video__overlay"
-                         style="background-image: url(&quot;${jsonObject.backgroundImage}&quot;);"></div>
-                    <div class="hero-video__readability"></div>
-                    <div class="hero-video__container">
-                        <div class="hero-video__container">
-                            <div class="hero-video__content l-spacer">
-                                <div class="hero-video__inner"><h1 class="hero-video__title">${jsonObject.title}</h1>
-                                    <div class="hero-video__description">${jsonObject.desciption}
-                                    </div>
-                                    <div class="c-movie-hero__actions">
-                                        <a href="#" class="c-button"><span>Online</span></a>
-                                        <a href="content.html" class="c-button"><span>Terug naar Content</span></a>
-                                        <a href="#" class="c-button" id="editButton"><span>Aanpassen</span></a>
-                                        <div class="selectseason"><label for="language" hidden>Select a Programming
-                                            Language:</label>
-                                            <select name="language" id="language">
-                                                <option value="javascript">JavaScript</option>
-                                                <option value="python">Python</option>
-                                                <option value="c++" disabled>C++</option>
-                                                <option value="java" selected>Java</option>
-                                            </select></div>
-                                    </div>
-                  </header>
-            </div>
-        </div>`
-    html_show.innerHTML = htmlstring_show;
-}
-
-
 const getShowById = function (id) {
     handleData(`https://goplayhowestapifunction.azurewebsites.net/api/getshowbyid?_id=${id}`, showShow);
-}
-
-const showMovie = function (jsonObject) {
-    console.log(jsonObject)
-    html_movie.innerHTML = ""
-    let htmlstring_movie = `
-            <div class="c-movie-hero c-movie-hero--program is-movie">
-                <div class="c-movie-hero__bg has-blur"
-                     style="background-image: url(&quot;${jsonObject.backgroundImage}&quot;);"></div>
-                <div class="c-movie-hero__bg-readable"></div>
-                <div class="c-movie-hero__container c-movie-hero__movie-container" style="margin-top: 25px">
-                    <div class="c-movie-hero-video__container">
-                        <div class="c-movie-hero-video__content l-spacer">
-                            <div class="c-movie-hero-video__inner"><h1 class="c-movie-hero-video__title" contenteditable="true">${jsonObject.title}</h1>
-                                <div class="c-movie-hero-video__description"><p>${jsonObject.description}</p>
-                                </div>
-                                <div class="c-movie-hero__actions">
-                                    <a class="c-button"
-                                       href="https://www.goplay.be${jsonObject.file_link}" target="_blank">
-                                        <span class="button__text">Afspelen</span>
-                                    </a>
-                                    <a href="#" class="c-button"><span>Online</span></a>
-                                    <a href="content.html" class="c-button"><span>Terug naar Content</span></a>
-                                    <a href="#" class="c-button" id="editButton"><span>Aanpassen</span></a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <button class="c-movie-hero__poster c-button--poster" type="button">
-                        <div class="play-button"></div>
-                        <picture class="l-spacer">
-                            <img src="${jsonObject.thumbnailImage}"
-                                 alt="Winter's Dream" width="510" height="740"></picture>
-                    </button>
-                </div>
-            </div>`
-    html_movie.innerHTML = htmlstring_movie;
-    document.querySelector(".c-movie-hero-video__title").addEventListener("input", function() {
-        console.log(this.innerHTML)
-    }, false);
 }
 
 const getMovieById = function (id) {
@@ -102,24 +10,18 @@ const editContent = function () {
     //split the url and take the 3 parameter
     const url = window.location.href;
     // check if you are on the movie/serie editing page
-    if (url.includes("edit_movie.html") || url.includes("edit_serie.html")) {
-        let id = url.split('?')[1]
-        //check if you are editing a movie or a serie
-        if (url.includes("edit_movie.html")) {
-            //fetch the data from the API
-            getMovieById(id)
-            html_movie = document.querySelector(".js-movie-data")
-        } else if (url.includes("edit_serie.html")) {
-            html_show = document.querySelector(".js-show-data")
-            getShowById(id)
+    if (url.includes("create_new_details.html")) {
+        let [url, id, movie] = window.location.href.split(/[?&]/);
+        if (!id) {
+            console.log("new show/movie")
+        } else if (id && movie === "movie=true") {
+            console.log("is movie")
+        } else if (id && movie === "movie=false") {
+            console.log("is tvshow")
         } else {
-            //no clue what would cause this to run
+            console.log(movie)
+            console.log("idk buddy you did something wrong")
         }
-        //select the buttons and check for click events
-        const editButton = document.querySelector("#editButton");
-        const cancelButton = document.querySelector("#cancelButton");
-        editButton.addEventListener('click', modalContentToggle);
-        cancelButton.addEventListener('click', modalContentToggle);
     } else {
         //do nothing cause not on the right page
     }
