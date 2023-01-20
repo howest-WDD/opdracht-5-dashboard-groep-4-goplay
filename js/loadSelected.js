98a3f30f0f2719d45f7740061209b8111ff6cfda
Nathan Dhaene <nathan.dhaene@student.howest.be>
2023-01-20 01:02:57 +0100

    functional delete button

--- js/loadSelected.js
+++ js/loadSelected.js
@@ -1,10 +1,30 @@
 'use strict';
+let idCount = 4 ;
 
+const listenToDelete = function(){
+  const deleteButton = document.querySelectorAll('.js-block-delete')
+
+  for(let button of deleteButton){
+    let buttonId = button.id;
+
+    button.addEventListener('click', function(){
+      deleteBlock(buttonId);
+    })
+  } 
+
+}
+
+const deleteBlock = function(childId){
+  let parent = document.querySelector('.c-startscherm__container');
+  let child = document.getElementById(`drag${childId}`)
+  child.remove();
+}
 
 const addBlocks = function(selection){
 
   setTimeout(() => {
     const container = document.querySelector('.c-startscherm__container');
+    
       let oldBlock = ``;
        oldBlock = container.innerHTML;
 
@@ -14,11 +34,11 @@ const addBlocks = function(selection){
       
 
 
-     newBlock = `<div class="c-draggable" draggable="true">
+     newBlock = `<div class="c-draggable" draggable="true" id="drag${idCount}">
                 
      <div class="c-input"> <h2><input type="text" value="${selection}"></h2>
 
-      <button class="c-users__delete js-sub-delete" id="js-block-delete">
+      <button class="c-users__delete js-block-delete" id="${idCount}">
         <svg style="width: 24px; height: 24px" class="c-users__deletesvg" viewBox="0 0 24 24">
           <path fill="currentColor" class="c-users__deletepath" d="M6,19A2,2 0 0,0 8,21H16A2,2 0 0,0 18,19V7H6V19M8.46,11.88L9.87,10.47L12,12.59L14.12,10.47L15.53,11.88L13.41,14L15.53,16.12L14.12,17.53L12,15.41L9.88,17.53L8.47,16.12L10.59,14L8.46,11.88M15.5,4L14.5,3H9.5L8.5,4H5V6H19V4H15.5Z"></path>
         </svg>
@@ -33,7 +53,7 @@ const addBlocks = function(selection){
         <div class="row">
         <div class="container">
             <button class="handle left-handle">
-              <div class="text">&#8249;</div>
+            <img src="./assets/svg/carouselLeft.svg" alt="arrow">
             </button>
             <div class="progress-bar"></div>
             <div class="slider c-carousel js-Carousel${selection}">
@@ -41,7 +61,7 @@ const addBlocks = function(selection){
               
             </div>
             <button class="handle right-handle">
-              <div class="text">&#8250;</div>
+            <img src="./assets/svg/carouselRight.svg" alt="arrow">
             </button>
             
           </div>
@@ -49,10 +69,11 @@ const addBlocks = function(selection){
     </div>`
 
    
-
+    idCount++ ;
+    
    container.innerHTML = ``;
    container.innerHTML = newBlock+oldBlock;
-
+   listenToDelete();
    addCarouselData(selection) 
    loadStartschermFuncties()
   }, 10);
@@ -77,7 +98,7 @@ const checkstartscherm = function () {
         let i = 0;
 
 	if (document.querySelector('.c-startscherm')) {
-        
+        listenToDelete();
         listenToButton();
 	} else {
 		console.log('no startscherm here');
